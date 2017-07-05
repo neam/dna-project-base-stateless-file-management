f2b0d364c7fa43b009e67348c9366484ccb23710
Fredrik Wollsén <fredrik@neam.se>
2017-07-05 09:15:46 +0300

    Support files for unit tests

---
+++ src/File.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace neam\stateless_file_management;
+
+/**
+ * Minimal implementation of a class that represents a File record, used for internal unit tests
+ */
+
+class File
+{
+
+    use FileTrait;
+
+}
--- tests/unit/FilestackIntegrationLogicTest.php
+++ tests/unit/FilestackIntegrationLogicTest.php
@@ -1,5 +1,6 @@
 <?php
 use Codeception\Util\Stub;
+use neam\stateless_file_management\File;
 
 class FilestackIntegrationLogicTest extends \Codeception\TestCase\Test
 {
